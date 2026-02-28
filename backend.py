c40e4a2e19978d2d456e9c915de6bffa8e1f07de
NicolasFay <njfay@uvm.edu>
2026-02-28 10:20:49 -0500

    added main response

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from database import db
 from typing import Optional
 import uvicorn
@@ -11,7 +12,10 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def login(request: Request):
-    return 
+    return templates.templates.TemplateResponse(
+        "dashboard.jinja",
+        {"request": request,}
+    )
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)
