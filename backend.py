24e72ca110227c4d76a0ee706a6301d36b4a79a7
NicolasFay <njfay@uvm.edu>
2026-02-28 09:59:31 -0500

    changed folder structure

--- backend.py
+++ backend.py
@@ -1,1 +1,8 @@
-import fastapi
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from database import db
+from typing import Optional
+
+app = FastAPI()
+app.mount("/frontend", StaticFiles(directory="frontend"), name="static")
