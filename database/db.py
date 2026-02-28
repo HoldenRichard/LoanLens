3894471f605d678b3a2a8740b9cb1ef91a02a06a
NicolasFay <njfay@uvm.edu>
2026-02-28 10:46:09 -0500

    example db function

--- database/db.py
+++ database/db.py
@@ -1,3 +1,17 @@
 # databse funcitonality
 
 # create functions to access data from .db file
+
+import sqlite3 as sql
+import matplotlib
+
+# get all users 
+def get_users():
+    connection = sql.connect("database/fintech.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT user_id, username, email FROM users ORDER BY username")
+    users = []
+    for row in cursor.fetchall():
+        users.append({"user_id": row[0], "username": row[1], "email": row[2]})
+    connection.close()
+    return users
