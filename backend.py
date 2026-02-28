5d1f02c3cd2f61ffd913c199f1023c34c221b795
NicolasFay <njfay@uvm.edu>
2026-02-28 10:15:01 -0500

    added dependecies

--- backend.py
+++ backend.py
@@ -7,7 +7,7 @@ import uvicorn
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def login(request: Request):
